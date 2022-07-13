fd5d36f44eb875e09c67fe9968092f9ba950d645
MirkOrlando <mirko@orlando-m.it>
2022-07-13 17:21:58 +0200

    7-Settaggio-back-parte-V close

---
+++ database/factories/StatisticFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Statistic;
+use Faker\Generator as Faker;
+
+$factory->define(Statistic::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
