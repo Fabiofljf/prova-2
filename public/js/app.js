4fb2089f7a45575f91a72167f21b0bdc9bb77f30
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-07-13 15:21:34 +0200

    1-Settaggio-scss+js#1-closed

--- public/js/app.js
+++ public/js/app.js
@@ -53429,8 +53429,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/Laravel/prova-2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Laravel/prova-2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\marco\Desktop\repodiprova\prova-2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\marco\Desktop\repodiprova\prova-2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
